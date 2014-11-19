ca49e1effd15e85c7e6a8708442a549e2f1d926b
danjocutler <danjocutler@gmail.com>
2014-11-19 16:13:23 +0000

    User can add a photo

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -15,15 +15,25 @@ describe 'photos' do
   context 'photos have been added' do
   
   before do
-    Photo.create(name: 'test_photo')
+    Photo.create(name: 'test photo')
   end
 
 	  it 'should display photos' do
 	    visit '/photos'
-	    expect(page).to have_content('test_photo')
+	    expect(page).to have_content('test photo')
 	    expect(page).not_to have_content('No photos')
 	  end
 
 	end
 
+	describe 'creating photos' do
+	 it 'prompts user to fill out a form, then displays the new photo' do
+	  visit '/photos'
+	  click_link 'Add a photo'
+	  fill_in 'Name', with: 'test photo'
+	  click_button 'Create Photo'
+	  expect(page).to have_content 'test photo'
+	  expect(current_path).to eq '/photos'
+	 end
+	end
 end
