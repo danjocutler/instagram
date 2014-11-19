2835ec264efc40cb50b69bb0d0bc9e991d4293f6
danjocutler <danjocutler@gmail.com>
2014-11-19 14:25:34 +0000

    can display 'photos' on homepage

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
 describe 'photos' do
+
   context 'no photos have been added' do
+
       it 'should display a prompt to add a photo' do
       visit '/photos'
       expect(page).to have_content 'No photos'
       expect(page).to have_link 'Add a photo'
     end
+
   end
+
+  context 'photos have been added' do
+  
+  before do
+    Photo.create(name: 'test_photo')
+  end
+
+	  it 'should display photos' do
+	    visit '/photos'
+	    expect(page).to have_content('test_photo')
+	    expect(page).not_to have_content('No photos')
+	  end
+
+	end
+
 end
